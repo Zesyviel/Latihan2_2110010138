e7b307632a40d0ee5a1a9b9f738cf7d0bb513173
Zesyviel <anggajstar@gmail.com>
2023-06-13 20:17:37 +0800

    Menjalankan Program

--- src/main/java/universitas/MahasiswaBeraksi.java
+++ src/main/java/universitas/MahasiswaBeraksi.java
@@ -3,6 +3,8 @@ package universitas;
 
 public class MahasiswaBeraksi {
     public static void main(String[] args) {
-        
+        // objek
+        Mahasiswa zet = new Mahasiswa("Zet Erlangga Sukaria", "2110010138", "Teknik Informatika");
+        zet.getDetail();
     }
 }
