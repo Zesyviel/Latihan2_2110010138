b6b756a9b3f9255be1b0ee9043d3eb55899db13e
Zesyviel <anggajstar@gmail.com>
2023-06-13 20:06:53 +0800

    Melengkapi class Mahasiswa

--- src/main/java/universitas/Mahasiswa.java
+++ src/main/java/universitas/Mahasiswa.java
@@ -1,8 +1,40 @@
 package universitas;
 
 public class Mahasiswa {
+    //varibale
     private String npm, nama, prodi;
-    
+    //construktor
+    public Mahasiswa(String npm, String nama, String prodi){
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+    //get and set
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    // Method
     public void getDetail(){
         System.out.println("Nama Mahasiswa  : " + npm);
         System.out.println("Npm Mahasiswa   : " + nama);
